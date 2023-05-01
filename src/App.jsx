5bbca16ad16467471e86633c7fa06436d6592b9d
dianaveselkin <vess_di@mail.ru>
2023-05-01 13:00:45 +0300

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { PostList } from './components/PostList';
 import data from './DB/data.json';
+import { PostPage } from './pages/PostPage';
+import { UserPage } from './pages/UserPost';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <PostList posts={data} />
+      {/* <PostPage /> */}
+      {/* <UserPage /> */}
       <Footer />
     </div>
   );
